c0fd5f56fd6e7b7a1816842250326ec8990625d2
sakdiDSDSDF <1945078472@qq.com>
2025-04-10 22:24:56 +0800

    .

---
+++ docs/javascripts/music.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = new Audio("/assets/music/塞壬唱片-MSR,BaoUner - 生命流.mp3"); // 音频文件路径
+    audio.loop = true; // 设置音乐循环播放
+    audio.play(); // 播放音乐
+});
